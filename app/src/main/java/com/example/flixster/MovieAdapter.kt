2b1cd3a50cccb9f79d679eadbf7087fb8d30f738
pelumiolutimehin <pelumiolutimehin@gmail.com>
2022-03-04 13:47:10 -0600

    Add click listener and YouTubePlayer Component

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,22 +9,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 private const val TAG = "MovieAdapter"
-
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>, private val config: Configuration)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),  View.OnClickListener {
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val tvTitleLand = itemView.findViewById<TextView>(R.id.tvTitleLand)
         private val tvOverviewLand = itemView.findViewById<TextView>(R.id.tvOverviewLand)
         private val ivBackdrop = itemView.findViewById<ImageView>(R.id.ivBackdrop)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
         fun bind(movie: Movie) {
+
             if(config.orientation == Configuration.ORIENTATION_PORTRAIT){
                 tvTitle.text = movie.title
                 tvOverview.text = movie.overview
@@ -35,6 +42,15 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 Glide.with(context).load(movie.backdropImageURL).into(ivBackdrop)
             }
         }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+//            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,7 +60,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.i(TAG, "onBindViewHolder posotion $position")
+        Log.i(TAG, "onBindViewHolder position $position")
         val movie = movies[position]
         holder.bind(movie)
     }
